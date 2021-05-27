8529a797a2f630cf361d83048d23137c3fcefd1c
froilanenriquez3 <fenriquezthree@gmail.com>
2021-05-27 11:25:47 +0200

    deploy

---
+++ images/Vue CLI App_files/app.04b1e6cc124c994c3f4b.hot-update.js
@@ -0,0 +1,39 @@
+webpackHotUpdate("app",{
+
+/***/ "../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/babel-loader/lib/index.js?!../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/cache-loader/dist/cjs.js?!../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/vue-loader/lib/index.js?!./components/SafetyVideo.vue?vue&type=script&lang=js&":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/cache-loader/dist/cjs.js?{\"cacheDirectory\":\"node_modules/.cache/vue-loader\",\"cacheIdentifier\":\"156349cc-vue-loader-template\"}!../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/vue-loader/lib/loaders/templateLoader.js?!../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/cache-loader/dist/cjs.js?!../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/vue-loader/lib/index.js?!./components/SafetyVideo.vue?vue&type=template&id=70178e3e&scoped=true&":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/css-loader/dist/cjs.js?!../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/vue-loader/lib/loaders/stylePostLoader.js!../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/postcss-loader/src/index.js?!../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/cache-loader/dist/cjs.js?!../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/vue-loader/lib/index.js?!./components/SafetyVideo.vue?vue&type=style&index=0&id=70178e3e&scoped=true&lang=css&":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/vue-style-loader/index.js?!../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/css-loader/dist/cjs.js?!../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/vue-loader/lib/loaders/stylePostLoader.js!../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/postcss-loader/src/index.js?!../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/cache-loader/dist/cjs.js?!../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/vue-loader/lib/index.js?!./components/SafetyVideo.vue?vue&type=style&index=0&id=70178e3e&scoped=true&lang=css&":
+false,
+
+/***/ "./assets/videos/video.mp4":
+false,
+
+/***/ "./components/SafetyVideo.vue":
+/*!************************************!*\
+  !*** ./components/SafetyVideo.vue ***!
+  \************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _usr_local_lib_node_modules_vue_cli_service_global_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/vue-loader/lib/runtime/componentNormalizer.js */ \"../../../../../../usr/local/lib/node_modules/@vue/cli-service-global/node_modules/vue-loader/lib/runtime/componentNormalizer.js\");\nvar render, staticRenderFns\nvar script = {}\n\n\n/* normalize component */\n\nvar component = Object(_usr_local_lib_node_modules_vue_cli_service_global_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(\n  script,\n  render,\n  staticRenderFns,\n  false,\n  null,\n  null,\n  null\n  \n)\n\ncomponent.options.__file = \"components/SafetyVideo.vue\"\n/* harmony default export */ __webpack_exports__[\"default\"] = (component.exports);//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9jb21wb25lbnRzL1NhZmV0eVZpZGVvLnZ1ZS5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL2NvbXBvbmVudHMvU2FmZXR5VmlkZW8udnVlP2YzYTUiXSwic291cmNlc0NvbnRlbnQiOlsidmFyIHJlbmRlciwgc3RhdGljUmVuZGVyRm5zXG52YXIgc2NyaXB0ID0ge31cblxuXG4vKiBub3JtYWxpemUgY29tcG9uZW50ICovXG5pbXBvcnQgbm9ybWFsaXplciBmcm9tIFwiIS4uLy4uLy4uLy4uLy4uLy4uLy4uL3Vzci9sb2NhbC9saWIvbm9kZV9tb2R1bGVzL0B2dWUvY2xpLXNlcnZpY2UtZ2xvYmFsL25vZGVfbW9kdWxlcy92dWUtbG9hZGVyL2xpYi9ydW50aW1lL2NvbXBvbmVudE5vcm1hbGl6ZXIuanNcIlxudmFyIGNvbXBvbmVudCA9IG5vcm1hbGl6ZXIoXG4gIHNjcmlwdCxcbiAgcmVuZGVyLFxuICBzdGF0aWNSZW5kZXJGbnMsXG4gIGZhbHNlLFxuICBudWxsLFxuICBudWxsLFxuICBudWxsXG4gIFxuKVxuXG5jb21wb25lbnQub3B0aW9ucy5fX2ZpbGUgPSBcImNvbXBvbmVudHMvU2FmZXR5VmlkZW8udnVlXCJcbmV4cG9ydCBkZWZhdWx0IGNvbXBvbmVudC5leHBvcnRzIl0sIm1hcHBpbmdzIjoiQUFBQTtBQUFBO0FBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./components/SafetyVideo.vue\n");
+
+/***/ }),
+
+/***/ "./components/SafetyVideo.vue?vue&type=script&lang=js&":
+false,
+
+/***/ "./components/SafetyVideo.vue?vue&type=style&index=0&id=70178e3e&scoped=true&lang=css&":
+false,
+
+/***/ "./components/SafetyVideo.vue?vue&type=template&id=70178e3e&scoped=true&":
+false
+
+})
